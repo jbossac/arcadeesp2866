f7cfd14a27f6faa7374b324f8468a28ee0b78f6b
Jorge <jbossa@geovictoria.com>
2022-01-15 23:46:50 +0000

    Tv Switch

--- arcadeesp8266.ino
+++ arcadeesp8266.ino
@@ -1,4 +1,10 @@
 #include <ESP8266WiFi.h>
+#include <IRsend.h>
+#include <ir_LG.h>
+
+const int IRPIN = D7;
+
+IRsend irsend(IRPIN);
 
 const char *ssid = "ffwifiubq2";
 const char *password = "hura-3613";
@@ -19,10 +25,23 @@ void setup()
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
+    irsend.begin();
+}
+
+void sendTvSwitch()
+{
+    uint64_t code = 0x20DF10EF;
+    irsend.sendNEC(code, 32);
+}
+
+void lightNotify(){
+    
 }
 
 void loop()
 {
     Serial.println("Hola prueba Wemos2");
+
+    Serial.println("Envio Code!");
     delay(1000);
 }
